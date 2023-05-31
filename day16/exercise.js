95ee4e030382f1d32093e95786b21c062c76aa0c
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-05-31 18:28:25 +0530

    upload file

---
+++ day16/exercise.js
@@ -0,0 +1,20 @@
+// 30-days-of-react-challenge: Day16: Higher Order Components
+
+// Exercise: Level 1:
+
+// 1. What is higher order function?
+// A higher-order function is a function that takes one or more functions as arguments and/or returns a function as its result.
+
+// 2. What is Higher Order Component?
+// A Higher Order Component (HOC) is a function that takes a component and returns a new component with additional functionality.
+
+// 3. What is the difference between higher order function and higher order component?
+/**  The main difference is in their application and purpose:
+
+Higher Order Function: It is a function that takes one or more functions as arguments or returns a function as its result.
+Higher Order Component: It is a function that takes a component as an argument and returns a new enhanced component with additional props or behavior.
+
+In summary, a higher order function operates on regular functions, whereas a higher order component operates on React components. */
+
+// 4. A higher order component can allow us to enhance a component. (T or F)
+// True. A higher order component can allow us to enhance a component.
